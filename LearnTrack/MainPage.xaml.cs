b8277b345324269b395a2f1b0667bd617dbbc63b
Paweł Ługowski <pawel.lugowski02@gmail.com>
2024-08-31 13:51:38 +0200

    Dodaj pliki projektów.

---
+++ LearnTrack/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace LearnTrack
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("Tytuł", "Test", "Ok");
+		}
+	}
+
+}
