a86b7abe63070e1555b22f39ed00767b5e45bd08
Paweł Ługowski <pawel.lugowski02@gmail.com>
2024-09-04 15:50:14 +0200

    Zmieniono wukład strony głównej i utworzono plik konfiguracyjny so sqlite

--- LearnTrack/Pages/MainPage.xaml.cs
+++ LearnTrack/Pages/MainPage.xaml.cs
@@ -28,4 +28,5 @@ public partial class MainPage : ContentPage
 	{
 
 	}
+
 }
---
+++ LearnTrack/SQLiteConstants.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace LearnTrack;
+
+public static class SQLiteConstants
+{
+	private const string DbFileName = "LearnTrackDb.db";
+	public const SQLiteOpenFlags Flags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+	public static string DatabasePath
+	{
+		get =>
+			Path.Combine(FileSystem.AppDataDirectory, DbFileName);
+	}
+}
